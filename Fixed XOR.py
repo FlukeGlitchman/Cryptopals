2f8e072f179b3ab365c94118638492e1c4ca8bab
Dagon <80639501+FlukeGlitchman@users.noreply.github.com>
2026-06-01 16:09:05 +0000

    first upload
    uploading the code of the first two challenges in cryptopals

---
+++ Fixed XOR.py
@@ -0,0 +1,11 @@
+
+from pwn import *
+X=bytes.fromhex('1c0111001f010100061a024b53535009181c')
+Y=bytes.fromhex('686974207468652062756c6c277320657965')
+xord=xor(X,Y)
+answer = bytes.hex(xord)
+correctanswer = '746865206b696420646f6e277420706c6179'
+if (answer == correctanswer):
+    print("correct")
+else:
+    print("hang yourself")
