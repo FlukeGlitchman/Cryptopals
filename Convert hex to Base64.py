1295f18ad5c68757ad66b9e8be9884861f58b5ef
Dagon <80639501+FlukeGlitchman@users.noreply.github.com>
2026-06-01 17:02:58 +0000

    Delete Convert hex to Base64.py

--- Convert hex to Base64.py
+++
@@ -1,6 +0,0 @@
-import base64
-from base64 import *
-real_answer = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-x="49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-answer=base64.b64encode(bytes.fromhex(x)).decode()
-print(answer)
